a5b91c638942090fdd4f21d93ad801615568c4e7
Martin Murphy <martin.murphy@whiteboard-it.com>
2010-08-25 04:35:12 -0500

    Code Clean up and comments.

--- LoveSeat/CouchClient.cs
+++ LoveSeat/CouchClient.cs
@@ -15,15 +15,23 @@ namespace LoveSeat
 		/// <param name="port">The port of the CouchDB instance</param>
 		/// <param name="username">The username of the CouchDB instance</param>
 		/// <param name="password">The password of the CouchDB instance</param>
-		public CouchClient(string host, int port, string username, string password):base(username, password)
+		public CouchClient(string host, int port, string username, string password)
+			: base(username, password)
 		{
 			baseUri = "http://" + host + ":" + port + "/";
 		}
 
+		/// <summary>
+		/// Triggers one way replication from the source to target.  If bidirection is needed call this method twice with the source and target args reversed.
+		/// </summary>
+		/// <param name="source">Uri or database name of database to replicate from</param>
+		/// <param name="target">Uri or database name of database to replicate to</param>
+		/// <param name="continuous">Whether or not CouchDB should continue to replicate going forward on it's own</param>
+		/// <returns></returns>
 		public JObject TriggerReplication(string source, string target, bool continuous)
 		{
 			var request = GetRequest(baseUri + "_replicate");
-			
+
 			var options = new ReplicationOptions(source, target, continuous);
 			var response = request.Post()
 				.Data(options.ToString())
@@ -32,27 +40,47 @@ namespace LoveSeat
 			return response.GetJObject();
 		}
 
-		
+
 
 		public JObject TriggerReplication(string source, string target)
 		{
 			return TriggerReplication(source, target, false);
 		}
 
+		/// <summary>
+		/// Creates a database
+		/// </summary>
+		/// <param name="databaseName">Name of new database</param>
+		/// <returns></returns>
 		public JObject CreateDatabase(string databaseName)
 		{
 			return GetRequest(baseUri + databaseName).Put().GetResponse().GetJObject();
 		}
-
+		/// <summary>
+		/// Deletes the specified database
+		/// </summary>
+		/// <param name="databaseName">Database to delete</param>
+		/// <returns></returns>
 		public JObject DeleteDatabase(string databaseName)
 		{
 			return GetRequest(baseUri + databaseName).Delete().GetResponse().GetJObject();
 		}
+
+		/// <summary>
+		/// Gets a Database object
+		/// </summary>
+		/// <param name="databaseName">Name of database to fetch</param>
+		/// <returns></returns>
 		public CouchDatabase GetDatabase(string databaseName)
 		{
-			return new CouchDatabase(baseUri , databaseName, username, password);
+			return new CouchDatabase(baseUri, databaseName, username, password);
 		}
 
+		/// <summary>
+		/// Returns a bool indicating whether or not the database exists.
+		/// </summary>
+		/// <param name="databaseName"></param>
+		/// <returns></returns>
 		public bool HasDatabase(string databaseName)
 		{
 			try
@@ -62,11 +90,12 @@ namespace LoveSeat
 					.GetResponse()
 					.GetJObject()["ok"];
 				return true;
-			}catch(Exception ex)
+			}
+			catch (Exception ex)
 			{
 				return false;
 			}
 		}
-		
+
 	}
 }
