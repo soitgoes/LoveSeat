6de5b02cef0fe0f2a625fd47417e0e011d7e0b44
Martin Murphy <martin.murphy@whiteboard-it.com>
2010-08-25 01:30:08 -0500

    credentials pulled out to config file.

--- LoveSeat.IntegrationTest/CouchClientTest.cs
+++ LoveSeat.IntegrationTest/CouchClientTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -10,11 +11,18 @@ namespace LoveSeat.IntegrationTest
 	public class CouchClientTest
 	{
 		private CouchClient client;
+		private const string baseDatabase = "love-seat-test-base";
+        private const string replicateDatabase = "love-seat-test-repli";
+
+		private readonly string host = ConfigurationManager.AppSettings["Host"].ToString();
+		private readonly int port = int.Parse(ConfigurationManager.AppSettings["Port"].ToString());
+		private readonly string username = ConfigurationManager.AppSettings["UserName"].ToString();
+		private readonly string password = ConfigurationManager.AppSettings["Password"].ToString();
 
 		[SetUp]
 		public void Setup()
 		{
-			client = new CouchClient("localhost", 5984, "bubba", "password");
+			client = new CouchClient(host , port, username, password);
 		}
 
 		[Test]
@@ -27,8 +35,15 @@ namespace LoveSeat.IntegrationTest
 		[Test]
 		public void CanTriggerReplication()
 		{
-			var obj  = client.TriggerReplication("http://bubba:password@remotedb.com:5984/remotedb", "local");
+			var obj  = client.TriggerReplication("http://Professor:Farnsworth@"+ host+":5984/" +replicateDatabase, baseDatabase);
 			Assert.IsTrue(obj != null);
 		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			//delete the test database
+		}
+
 	}
 }
