fa8ce5289fbd43506197f59247f021cd4ddc4518
Tomi <bosak.tomas@gmail.com>
2010-12-16 11:59:58 +0100

    Forgot to remove commented old code.

--- LoveSeat/Support/KeyOptions.cs
+++ LoveSeat/Support/KeyOptions.cs
@@ -9,19 +9,6 @@ using Newtonsoft.Json.Linq;
 
 namespace LoveSeat
 {
-    /*public interface IKeyOptions
-    {       
-        string ToString();
-        void Insert(int index, JToken item);
-        void RemoveAt(int index);
-        void Add(JToken item);
-        bool Remove(JToken item);
-        int Count { get; }
-        bool HasValues { get; }
-        void Add(object content);
-        void Add()
-    }*/
-
     public class KeyOptions : IKeyOptions
     {
         private JArray objects;
