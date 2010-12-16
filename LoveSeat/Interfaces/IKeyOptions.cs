2468c4706994e88af5963995b257fef2583d8470
Tomi <bosak.tomas@gmail.com>
2010-12-16 11:56:02 +0100

    Fixed IkeyOptions interface which didn't have Add method declaration with multiple parameters, therefore adding multiple (Start/End)Key params didn't work. This interface was also moved to Interfaces folder.

---
+++ LoveSeat/Interfaces/IKeyOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace LoveSeat.Interfaces
+{
+    public interface IKeyOptions
+    {
+        string ToString();
+        void Insert(int index, JToken item);
+        void RemoveAt(int index);
+        void Add(JToken item);
+        bool Remove(JToken item);
+        int Count { get; }
+        bool HasValues { get; }
+        void Add(object content);
+        void Add(params object[] items);
+    }
+}
