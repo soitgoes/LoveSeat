b5792561ba7547a254601a090221200d5fba2caf
Ian1971 <ian.cox@releaseconsulting.co.uk>
2013-10-15 13:34:40 +0100

    update json.net

---
+++ LoveSeat/Support/CookieStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace LoveSeat.Support
+{
+	public static class CookieStore
+	{
+		private static TtlDictionary<string, Cookie> cookiestore = new TtlDictionary<string, Cookie>();
+
+		public static TtlDictionary<string, Cookie> Cookiestore
+		{
+			get { return cookiestore; }
+			set { cookiestore = value; }
+		}
+
+		public static void Clear()
+		{
+			cookiestore = new TtlDictionary<string, Cookie>();
+		}
+	}
+}
