7e0ccb7398417962b09695b82ff07ed78e6cae9a
Ian1971 <ian.cox@releaseconsulting.co.uk>
2013-10-22 08:57:49 +0100

    add missing file

---
+++ LoveSeat/Support/NameValueCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+
+namespace LoveSeat.Support
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static string ToQueryString(this NameValueCollection collection)
+        {
+            var sb = new StringBuilder();
+
+            bool first = true;
+
+            foreach (string key in collection.AllKeys)
+            {
+                foreach (string value in collection.GetValues(HttpUtility.UrlEncode(key)))
+                {
+                    if (!first)
+                    {
+                        sb.Append("&");
+                    }
+
+                    sb.AppendFormat("{0}={1}", Uri.EscapeDataString(key), Uri.EscapeDataString(value));
+
+                    first = false;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static NameValueCollection AddReturn(this NameValueCollection target, NameValueCollection addFrom)
+        {
+            target.Add(addFrom);
+            return target;
+        }
+    }
+}
